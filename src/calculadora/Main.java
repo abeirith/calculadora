b2361603e8df01968577c563935e2afaf448d661
Alexandre Beirith <beirith@beirith-Notebook.(none)>
2013-05-01 21:04:01 -0300

    Adicionando novos arquivos

---
+++ src/calculadora/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author beirith
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        Calculadora calci = new Calculadora();
+        calci.setVisible(true);
+
+    }
+}
